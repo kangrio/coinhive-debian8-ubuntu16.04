c0cd259433bb17d4fb01cb362b24ba25682eb9eb
KangRio <kang.rio123@gmail.com>
2017-12-05 08:20:47 +0700

    Add files via upload

---
+++ Install-coinhive.sh
@@ -0,0 +1,35 @@
+
+#update
+sudo apt-get update
+
+# install wget and curl
+sudo apt-get update;apt-get -y install wget curl;
+
+# set time GMT +7
+ln -fs /usr/share/zoneinfo/Asia/Bangkok /etc/localtime
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+service ssh restart
+
+#set time reboot server
+echo "0 0 * * * root /usr/bin/reboot" > /etc/cron.d/reboot
+
+#install node8.json
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Install latest chrome and puppeteer dependencies
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - &&\
+sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' &&\
+apt-get update &&\
+apt-get install -y google-chrome-unstable gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+
+# Install coin-hive
+npm i -g coin-hive --unsafe-perm=true --allow-root
+
+#autorun
+wget -O /etc/rc.local "https://raw.githubusercontent.com/kangrio/coinhive-ubuntu16.04/master/rc.local"
+
+#start
+reboot
